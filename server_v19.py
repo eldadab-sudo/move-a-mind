cfe49602019660dd182099e21c5419a4bc1dc5c1
eldadab-sudo <eldadab@gmail.com>
2026-09-09 23:19:10 +0300

    Shorten start button label

---
+++ server_v19.py
@@ -0,0 +1,16 @@
+import os
+import server_v18 as v18
+from http.server import ThreadingHTTPServer
+
+app = v18.app
+
+index_path = app.WEB / 'index.html'
+html = index_path.read_text(encoding='utf-8')
+html = html.replace('התחל סימולציה','התחל')
+html = html.replace('בדוק אותי','התחל')
+index_path.write_text(html, encoding='utf-8')
+
+if __name__ == '__main__':
+    os.chdir(app.ROOT)
+    print('Move A Mind v2.3 start button label')
+    ThreadingHTTPServer(('0.0.0.0', app.PORT), v18.H).serve_forever()
