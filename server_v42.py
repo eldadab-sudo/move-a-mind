556b929804e8f1068ed5ae2e1e66841af00312b3
eldadab-sudo <eldadab@gmail.com>
2026-09-18 07:58:12 +0300

    Mark checkout recovery production version

--- server_v42.py
+++ server_v42.py
@@ -87,5 +87,5 @@ class H(v41.H):
 
 if __name__ == '__main__':
     os.chdir(app.ROOT)
-    print('Move A Mind v4.28 - concrete context-grounded conversation engine')
+    print('Move A Mind v4.30 - grounded engine + checkout state recovery')
     ThreadingHTTPServer(('0.0.0.0', app.PORT), H).serve_forever()
