5965fcf54dd63c8a4a2b30317370739ee4e4bd7c
eldadab-sudo <eldadab@gmail.com>
2026-09-18 08:00:10 +0300

    Mark checkout recovery v4.36

--- server_v42.py
+++ server_v42.py
@@ -87,5 +87,5 @@ class H(v41.H):
 
 if __name__ == '__main__':
     os.chdir(app.ROOT)
-    print('Move A Mind v4.35 - validated Stripe checkout + persistent report return')
+    print('Move A Mind v4.36 - Stripe checkout + persistent report return')
     ThreadingHTTPServer(('0.0.0.0', app.PORT), H).serve_forever()
