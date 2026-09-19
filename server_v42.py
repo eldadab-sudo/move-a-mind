a1b00270fcf7b0adf288f9fd5317ef448132413b
eldadab-sudo <eldadab@gmail.com>
2026-09-19 23:48:51 +0300

    Deploy analysis recovery v4.37

--- server_v42.py
+++ server_v42.py
@@ -87,5 +87,5 @@ class H(v41.H):
 
 if __name__ == '__main__':
     os.chdir(app.ROOT)
-    print('Move A Mind v4.36 - Stripe checkout + persistent report return')
+    print('Move A Mind v4.37 - analysis recovery + context engine')
     ThreadingHTTPServer(('0.0.0.0', app.PORT), H).serve_forever()
