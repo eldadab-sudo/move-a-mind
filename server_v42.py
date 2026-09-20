b9e926171ac52246690b9d4c256f8dbaea17d1c2
eldadab-sudo <eldadab@gmail.com>
2026-09-20 10:42:53 +0300

    Deploy adaptive usage tracking and rich preview

--- server_v42.py
+++ server_v42.py
@@ -140,5 +140,5 @@ class H(v41.H):
 
 if __name__ == '__main__':
     os.chdir(app.ROOT)
-    print('Move A Mind v4.41 - adaptive scenarios + rich preview')
+    print('Move A Mind v4.41.1 - adaptive scenarios + rich preview')
     ThreadingHTTPServer(('0.0.0.0', app.PORT), H).serve_forever()
